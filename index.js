eef246b7812f9e7e9424188b00a3b16ca6521cd7
Abel Zazueta <abel.zazueta@gmail.com>
2022-03-22 15:00:31 -0700

    Tests passed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun (toDo="roller-skate"){
+    return `This Saturday, I want to ${toDo}!`;
+}
+function mondayWork (toDo="go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+function wrapAdjective(symbol="*"){
+    return function (youAre = "special"){
+        return `You are ${symbol}${youAre}${symbol}!`;
+    }
+}
